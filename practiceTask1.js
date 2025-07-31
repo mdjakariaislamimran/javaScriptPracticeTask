50ddecb3e4a55d5817b623a37762851b69a48861
mdjakariaislamimran <mdjakariaislam.9423@gmail.com>
2025-07-31 18:15:46 +0600

    first task

---
+++ practiceTask1.js
@@ -0,0 +1,14 @@
+const moneyIHave = 1000;
+const orangePrice = 300;
+const applePrice = 200;
+console.log("I have " + moneyIHave + " Taka ");
+
+console.log("Orange 1kg price is " + orangePrice + " Taka");
+
+console.log("Apple 1kg price is " + applePrice + " Taka ");
+
+const totalPrice = orangePrice + applePrice;
+console.log("Total price of 1kg Orange and 1kg Apple is " + totalPrice + " Taka");
+
+const shopKeeperWillReturnMyChange = moneyIHave - totalPrice;
+console.log("Shopkeeper will return my change: " + shopKeeperWillReturnMyChange + " Taka"); 
